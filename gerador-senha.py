b1fca0c7f84f9ee49f1632e90273148c6c3875dc
Rafael Fernandes <rafaelduartesfns@gmail.com>
2023-05-20 01:38:08 -0300

    codigos

--- gerador-senha.py
+++ gerador-senha.py
@@ -1,15 +1,19 @@
 import secrets
-import random as rn
 import string
-
-letras = string.ascii_letters
-digitos = string.digits
-chars_espec = string.punctuation
-
-alfabeto = letras + digitos + chars_espec
 
 class tam_senha():
     def __init__(self):
-        num = self
+        letras = string.ascii_letters
+        digitos = string.digits
+        chars_espec = string.punctuation
+
+        alfabeto = letras + digitos + chars_espec
+
+        senha = ""
+
+        tamanho_senha = self
+        for i in range(tamanho_senha):
+            senha += "".join(secrets.choice(alfabeto))
+        print(senha)
 
 
