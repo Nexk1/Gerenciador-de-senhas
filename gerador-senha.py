b1db2edf8d9e75c5083c0fc2e7c1b37842028a07
Rafael Fernandes <rafaelduartesfns@gmail.com>
2023-05-20 01:34:38 -0300

    codigos

---
+++ gerador-senha.py
@@ -0,0 +1,15 @@
+import secrets
+import random as rn
+import string
+
+letras = string.ascii_letters
+digitos = string.digits
+chars_espec = string.punctuation
+
+alfabeto = letras + digitos + chars_espec
+
+class tam_senha():
+    def __init__(self):
+        num = self
+
+
